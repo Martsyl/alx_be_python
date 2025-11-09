0d5b3d09db10dd3454ccbdca6f50dad692a3378e
martsyl <msnonuorah3964@gmail.com>
2025-11-09 06:01:49 +0100

    first task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10 
+number2 = 5
+# Addition
+sum_result = number1 + number2
+print("Addition of " number1 "and " number2, sum_result)
+
+# Subtraction
+subtraction_result = number1 - number2
+print("Subtraction of " number1 "and " number2, subtraction_result)
+
+# Multiplication
+multiplication_result = number1 * number2   
+print("Multiplication of " number1 "and " number2, multiplication_result)
