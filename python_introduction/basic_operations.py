5d614bc5cba8a63eae7943d4b8dd21db245c57ee
martsyl <msnonuorah3964@gmail.com>
2025-11-09 06:08:11 +0100

    fixed the runtime error

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,12 +2,12 @@ number1 = 10
 number2 = 5
 # Addition
 sum_result = number1 + number2
-print("Addition of " number1 "and " number2, sum_result)
+print(f"Addition of {number1} and {number2}: {sum_result}")
 
 # Subtraction
 subtraction_result = number1 - number2
-print("Subtraction of " number1 "and " number2, subtraction_result)
+print(f"Subtraction of {number1} and {number2}: {subtraction_result}")
 
 # Multiplication
 multiplication_result = number1 * number2   
-print("Multiplication of " number1 "and " number2, multiplication_result)
+print(f"Multiplication of {number1} and {number2}: {multiplication_result}")
