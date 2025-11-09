d3dd9dfa2f5fd3b4559f3f5b61486acce68db139
martsyl <msnonuorah3964@gmail.com>
2025-11-09 06:14:13 +0100

    created a new file and worked on the simple intrest formula

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000  # The initial amount of money
+rate = 0.05          # The annual interest rate in percentage
+time = 3          # The time in years
+# Calculate simple interest
+i = principal * rate * time
+print(f"The simple interest is: {i}")
